21c9808224609840b92d62a6e207d5cbf9a504a9
ioggstream <robipolli@gmail.com>
2011-11-06 19:31:27 +0100

    first import

---
+++ cryptlib.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+# coding=utf8
+# author robipolli@gmail.com
+# License AGPLv3 - http://www.gnu.org/licenses/agpl.html
+#
+# TODO use unittest
+
+from Crypto.Cipher import AES
+import binascii
+import re,os,sys
+from random import randint
+import unicodedata as ud
+
+
+
+class Cifratore:
+	"""
+		class to encrypt/decrypt with AES
+		file and directories, name included
+	"""
+	key = None
+	encryptor = None
+	mode = AES.MODE_ECB
+	re_encrypted = re.compile('[A-Z0-9]+$')
+	pads = 16
+
+	def __init__(self,key,mode=AES.MODE_ECB, pads=16):
+		self.key = key + '\0'*(32-len(key))
+		self.encryptor = AES.new(self.key, self.mode)
+
+	def encrypt_raw(self,s):
+		"""
+			encrypt s, previously padding it
+		"""
+		return self.encryptor.encrypt(self.pad(s))
+
+	def decrypt_raw(self,s):
+		"""
+			decrypt s and unpad it
+		"""
+		return self.unpad(self.encryptor.decrypt(s))
+
+	def encrypt(self, s):
+		"""
+			encrypt a string (simple or unicode)
+			after mangling it a bit to be compatible
+			with java.crypto encoder/decoder
+		"""
+		if isinstance(s,unicode):
+			s=s.encode("utf-8")
+		ps = self.s2us(s)
+		ps = self.encrypt_raw(ps)
+		return binascii.hexlify(ps).upper()
+		
+	def decrypt(self, c):
+		"""
+			encrypt a string (simple or unicode)
+			after mangling it a bit to be compatible
+			with java.crypto encoder/decoder
+		"""
+		cleartext = self.decrypt_raw(binascii.unhexlify(c))
+		return self.us2s(cleartext)
+	
+	def pad(self,s):
+		l = len(s) % self.pads
+		a = chr(self.pads-l) * (self.pads-l)
+		return s+a
+
+	def unpad(self,s):
+		return s[0:-ord(s[-1])]
+	
+	def us2s(self,s):
+		"""
+			convert a string containing unicode data to an unicode string
+		"""
+		return s.decode("utf-8").encode("unicode-escape").decode("string-escape")	
+
+	def s2us(self,s):
+		"""
+			convert a string containig special chars to a string 
+			containing unicode chars
+		"""
+		return s.encode("string-escape").decode("unicode-escape").encode("utf-8")
+
+
+
+key = 'secret'
+def testCryptFile():
+	"""
+	Use this test as usage manual
+	"""
+	file = "test.dat"
+	dest = "test.dat.aes"
+	e = Cifratore(key=key)
+
+	data = open(file,"r").read()
+	cdata = e.encrypt_raw(data)
+
+	ofd = open(dest,"w")
+	ofd.write(cdata)
+	ofd.close()
+
+	data1 = e.decrypt_raw(open(dest,"r").read())
+	assert (data1==data)
+	os.unlink(dest)
+
+def testCryptString():
+	for s in ["", "pippo", "Pluto123", "a\nbc" ]:
+		e = Cifratore(key=key)
+		assert(s == e.decrypt(e.encrypt(s)))
+		
+def testCryptUnicodeString():
+	for s in [u"òàè", "pòàòippo", "Pllòàuto123", "a\òàlnbc" ]:
+		e = Cifratore(key=key)
+		dec = e.decrypt(e.encrypt(s))
+		if isinstance(s,unicode):
+			assert(ud.normalize('NFC',s) == ud.normalize('NFC',dec.decode("utf-8")))
+		else:
+			assert(s==dec)
+		
+
+
+if __name__=='__main__':
+	print "running tests"
+	testCryptFile()
+	testCryptString()
+	testCryptUnicodeString()
+	print "tests ok"
